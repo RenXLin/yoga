0b24ceabb0710fb54dbfdc86f447501bb1093f4e
xren <xren@eclassmate.cn>
2014-07-10 00:30:18 +0800

    yoga
    瑜伽魔方

---
+++ yoga/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  yoga
+//
+//  Created by renxlin on 14-7-10.
+//  Copyright (c) 2014年 任小林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
