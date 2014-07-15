1ef46667b8146e925df9b79c2adfba6932432273
xren <xren@eclassmate.cn>
2014-07-15 22:29:47 +0800

    ok

--- yoga/RL_LoginViewController.m
+++ yoga/RL_LoginViewController.m
@@ -105,82 +105,78 @@
 {
     NSLog(@"%ld",(long)btn.tag);
     if (btn.tag == 2) {
-        //        NSDictionary *parameter = [NSDictionary dictionaryWithObjectsAndKeys:_account.text,@"username",_passWord.text,@"password",nil];
-        AFHTTPRequestOperationManager *manager = [[AFHTTPRequestOperationManager alloc] init];
-        manager.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"text/html"];
-        [manager GET:[NSString stringWithFormat:@"%@username=%@&password=%@",LOGIN_url,_account.text,_passWord.text] parameters:nil  success:^(AFHTTPRequestOperation *operation, id responseObject) {
-            NSLog(@"success");
+    AFHTTPRequestOperationManager *manager = [[AFHTTPRequestOperationManager alloc] init];
+    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"text/html"];
+    NSString *URLStr = [NSString stringWithFormat:LOGIN_url,_account.text,_passWord.text];
+    //待加入缓冲提示：
+    [manager GET:URLStr parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
             
-            NSString *URLStr = [NSString stringWithFormat:LOGIN_url,_account.text,_passWord.text];
-            //      待加入缓冲提示：
-            [manager GET:URLStr parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+            NSLog(@"%@",responseObject);
+            if ([[responseObject objectForKey:@"code"] intValue] == 200) {
+                NSLog(@"登陆成功");
                 
-                NSLog(@"%@",responseObject);
-                if ([[responseObject objectForKey:@"code"] intValue] == 200) {
-                    NSLog(@"登陆成功");
-                    
-                    UserInfo *userInfo = [UserInfo shareUserInfo];
-                    userInfo.token = [[responseObject objectForKey:@"data"] objectForKey:@"token"];
-                    userInfo.userName = [[responseObject objectForKey:@"data"] objectForKey:@"username"];
-                    //返回上级视图：
-                    [self dismissViewControllerAnimated:YES completion:nil];
-                }else{
-                    UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"提示" message:@"登陆失败" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
-                    [alert show];
-                }
-            } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                UserInfo *userInfo = [UserInfo shareUserInfo];
+                userInfo.token = [[responseObject objectForKey:@"data"] objectForKey:@"token"];
+                userInfo.userName = [[responseObject objectForKey:@"data"] objectForKey:@"username"];
+                //返回上级视图：
+                [self dismissViewControllerAnimated:YES completion:nil];
+            }else{
                 UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"提示" message:@"登陆失败" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
                 [alert show];
-            }];
-        }else if(btn.tag == 1){
-            RL_RegistViewController *rvc =[[RL_RegistViewController alloc] init];
-            [self presentViewController:rvc animated:YES completion:nil];
-        }
-         
-         }
-         
-         - (void)didReceiveMemoryWarning
-        {
-            [super didReceiveMemoryWarning];
-            // Dispose of any resources that can be recreated.
-            
-            
-        }
-         
-         -(UIView *)myNavgationBar:(CGRect)rect andTitle:(NSString *)tit
-        {
-            UIView *view = [[UIView alloc] initWithFrame:rect];
-            view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"title.png"]];
-            
-            //back button
-            UIButton *back = [UIButton buttonWithType:UIButtonTypeCustom];
-            back.frame = CGRectMake(2, 0, 40, view.frame.size.height);
-            [back addTarget:self action:@selector(backBtnClick) forControlEvents:UIControlEventTouchUpInside];
-            [back setImage:[UIImage imageNamed:@"title_icon.png"] forState:UIControlStateNormal];
-            [view addSubview:back];
-            
-            //title
-            UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(back.frame.size.width, 0, 70, rect.size.height)];
-            title.text = tit;
-            title.textColor = [UIColor colorWithRed:0.92f green:0.92f blue:0.92f alpha:1.00f];
-            [view addSubview:title];
-            return view;
-        }
-         
-         -(void)backBtnClick
-        {
-            [self dismissViewControllerAnimated:YES completion:nil];
-        }
-         
-        /*
-         #pragma mark - Navigation
-         
-         // In a storyboard-based application, you will often want to do a little preparation before navigation
-         - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-         {
-         // Get the new view controller using [segue destinationViewController].
-         // Pass the selected object to the new view controller.
-         }
-         */
-         
+            }
+        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+            UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"提示" message:@"登陆失败" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+            [alert show];
+        }];
+    }else if(btn.tag == 1){
+        RL_RegistViewController *rvc =[[RL_RegistViewController alloc] init];
+        [self presentViewController:rvc animated:YES completion:nil];
+    }
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+             
+             
+}
+
+-(UIView *)myNavgationBar:(CGRect)rect andTitle:(NSString *)tit
+{
+    UIView *view = [[UIView alloc] initWithFrame:rect];
+    view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"title.png"]];
+    
+    //back button
+    UIButton *back = [UIButton buttonWithType:UIButtonTypeCustom];
+    back.frame = CGRectMake(2, 0, 40, view.frame.size.height);
+    [back addTarget:self action:@selector(backBtnClick) forControlEvents:UIControlEventTouchUpInside];
+    [back setImage:[UIImage imageNamed:@"title_icon.png"] forState:UIControlStateNormal];
+    [view addSubview:back];
+    
+    //title
+    UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(back.frame.size.width, 0, 70, rect.size.height)];
+    title.text = tit;
+    title.textColor = [UIColor colorWithRed:0.92f green:0.92f blue:0.92f alpha:1.00f];
+    [view addSubview:title];
+    return view;
+}
+-(void)backBtnClick
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+/*
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+ {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
+
          @end
