06cf7d3ffe451157bec6fdccbf5bc74da47eaf28
xren <xren@eclassmate.cn>
2014-07-31 01:16:30 +0800

    social

--- yoga/AppDelegate.m
+++ yoga/AppDelegate.m
@@ -31,22 +31,22 @@
     // Override point for customization after application launch.
     //友盟分享：
     [UMSocialConfig setSupportedInterfaceOrientations:UIInterfaceOrientationMaskAll];
-
+    
     [UMSocialData setAppKey:@"53d4c20456240b2af4103c08"];
     
     //设置微信AppId，设置分享url，默认使用友盟的网址
-    [UMSocialWechatHandler setWXAppId:@"wx135514f2491ecfe0" url:@"http://www.umeng.com/social"];
-//
-//    //打开新浪微博的SSO开关
-    [UMSocialSinaHandler openSSOWithRedirectURL:@"http://sns.whalecloud.com/sina2/callback"];
-//
-//    //打开腾讯微博SSO开关，设置回调地址
-    [UMSocialTencentWeiboHandler openSSOWithRedirectUrl:@"http://sns.whalecloud.com/tencent2/callback"];
+    //    [UMSocialWechatHandler setWXAppId:@"wx135514f2491ecfe0" url:@"http://www.umeng.com/social"];
+    //
+    //    //打开新浪微博的SSO开关
+    //    [UMSocialSinaHandler openSSOWithRedirectURL:@"http://sns.whalecloud.com/sina2/callback"];
+    //
+    //    //打开腾讯微博SSO开关，设置回调地址
+    //    [UMSocialTencentWeiboHandler openSSOWithRedirectUrl:@"http://sns.whalecloud.com/tencent2/callback"];
     
     //设置分享到QQ空间的应用Id，和分享url 链接
-    [UMSocialQQHandler setQQWithAppId:@"801508461" appKey:@"298deebc09032d2e14288c81abd2b9fd" url:@"http://www.umeng.com/social"];
- 
-
+    //    [UMSocialQQHandler setQQWithAppId:@"801508461" appKey:@"298deebc09032d2e14288c81abd2b9fd" url:@"http://www.umeng.com/social"];
+    
+    
     //设置支持没有客户端情况下使用SSO授权
     [UMSocialQQHandler setSupportWebView:YES];
     
